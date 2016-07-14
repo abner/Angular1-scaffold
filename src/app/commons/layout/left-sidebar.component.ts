7ff9ee698e7d4422331e61e9ee999e924b196706
Abner Oliveira <abner.silva@gmail.com>
2016-07-14 00:55:22 -0300

    added page header

--- src/app/commons/layout/left-sidebar.component.ts
+++ src/app/commons/layout/left-sidebar.component.ts
@@ -3,7 +3,8 @@ import { Component } from 'ng-metadata/core';
 @Component({
     selector: 'main-left-sidebar',
     template: require('./_left-sidebar.html'),
-    styles: require('./left-sidebar.scss')
+    styles: require('./left-sidebar.scss'),
+    legacy: {}
 })
 export class MainLeftSidebarComponent {
     constructor() {
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,19 +1,28 @@
 import { Component, Inject, OnInit } from 'ng-metadata/core';
 import { DASHBOARD_BOXES_COMPONENTS } from '../dashboard';
 
+import { SessionService } from '../../commons/auth';
+
+import { User } from '../../commons/models';
+
 @Component({
     selector: 'main-component',
     template: require('./_main.html'),
+    styles: require('./main.scss'),
     directives: [...DASHBOARD_BOXES_COMPONENTS]
 })
 export class MainComponent implements OnInit {
     content: string = 'Algum conteúdo aqui!';
+    currentUser: User;
     currentNavItem: string = '';
     constructor(
         @Inject('$log') private $log: ng.ILogService,
-        @Inject('$state') private $state: ng.ui.IStateService
+        @Inject('$state') private $state: ng.ui.IStateService,
+        @Inject(SessionService) sessionService: SessionService
     ) {
         $log.info('MainComponent constructed!');
+
+        this.currentUser = sessionService.currentUser;
     }
 
     ngOnInit() {
